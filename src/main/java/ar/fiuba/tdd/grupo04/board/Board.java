a67eccec41e589da516fdee5c5852815803e0b25
Francisco López Destain <francisco.lopez@fdvsolutions.com>
2016-10-15 17:29:12 -0300

    Fix duplicate code in game that will be loaded from json

--- src/main/java/ar/fiuba/tdd/grupo04/board/Board.java
+++ src/main/java/ar/fiuba/tdd/grupo04/board/Board.java
@@ -9,15 +9,20 @@ public class Board<T> implements IBoard<T> {
 
     public Board(Integer rows, Integer columns) {
         this.cells = new ArrayList<>();
-        this.initCells(rows, columns);
+        this.initCells(rows, columns, Optional.empty());
     }
 
-    private void initCells(Integer rows, Integer columns) {
+    public Board(Integer rows, Integer columns, T initialValue) {
+        this.cells = new ArrayList<>();
+        this.initCells(rows, columns, Optional.ofNullable(initialValue));
+    }
+
+    private void initCells(Integer rows, Integer columns, Optional<T> initialValue) {
         for (Integer row = 0; row < rows; row++) {
             List rowList = new ArrayList<>();
             this.cells.add(rowList);
             for (Integer column = 0; column < columns; column++) {
-                rowList.add(Optional.empty());
+                rowList.add(initialValue);
             }
         }
     }
--- src/main/java/ar/fiuba/tdd/grupo04/games/Sudoku.java
+++ src/main/java/ar/fiuba/tdd/grupo04/games/Sudoku.java
@@ -12,41 +12,41 @@ import ar.fiuba.tdd.grupo04.rule.condition.AllLesserThanCondition;
 import ar.fiuba.tdd.grupo04.rule.condition.UniqueCondition;
 
 public class Sudoku {
-    IGame game;
-    private Board board;
-
-    public Sudoku() {
-        createGame();
-        createBoard();
-    }
-
-    private void createGame() {
-        game = new Game();
-        // Esto se levanta del json de juego
-        board = new Board(9, 9);
-        game.setBoard(board);
-        game.addRule(new Rule<>(new ColumnsCollector<>(board), new UniqueCondition()));
-        //game.addRule(new Rule<>(new RowsCollector<>(board), new UniqueCondition()));
-        //game.addRule(new Rule<>(new BlocksCollector<>(board), new UniqueCondition()));
-        game.addRule(new Rule<>(new AllCollector<>(board, 10), new AllLesserThanCondition()));
-        game.addRule(new Rule<>(new AllCollector<>(board, 0), new AllGreaterThanCondition()));
-    }
-
-    private void createBoard() {
-        // Esto se levanta del json de escenario
-        game.fillCell(new Coordinate(1, 1), 1);
-        game.fillCell(new Coordinate(3, 1), 2);
-        game.fillCell(new Coordinate(1, 3), 1);
-        game.fillCell(new Coordinate(1, 2), 1);
-        game.fillCell(new Coordinate(5, 1), 4);
-    }
-
-    public void playGame() {
-        //while(!game.winCondition() && !game.loseCondition()){
-        while (game.checkRules()) {
-            //tomar input
-            game.fillCell(new Coordinate(2, 7), 8);
-        }
-
-    }
+//    IGame game;
+//    private Board board;
+//
+//    public Sudoku() {
+//        createGame();
+//        createBoard();
+//    }
+//
+//    private void createGame() {
+//        game = new Game();
+//        // Esto se levanta del json de juego
+//        board = new Board(9, 9);
+//        game.setBoard(board);
+//        game.addRule(new Rule<>(new ColumnsCollector<>(board), new UniqueCondition()));
+//        //game.addRule(new Rule<>(new RowsCollector<>(board), new UniqueCondition()));
+//        //game.addRule(new Rule<>(new BlocksCollector<>(board), new UniqueCondition()));
+//        game.addRule(new Rule<>(new AllCollector<>(board, 10), new AllLesserThanCondition()));
+//        game.addRule(new Rule<>(new AllCollector<>(board, 0), new AllGreaterThanCondition()));
+//    }
+//
+//    private void createBoard() {
+//        // Esto se levanta del json de escenario
+//        game.fillCell(new Coordinate(1, 1), 1);
+//        game.fillCell(new Coordinate(3, 1), 2);
+//        game.fillCell(new Coordinate(1, 3), 1);
+//        game.fillCell(new Coordinate(1, 2), 1);
+//        game.fillCell(new Coordinate(5, 1), 4);
+//    }
+//
+//    public void playGame() {
+//        //while(!game.winCondition() && !game.loseCondition()){
+//        while (game.checkRules()) {
+//            //tomar input
+//            game.fillCell(new Coordinate(2, 7), 8);
+//        }
+//
+//    }
 }
