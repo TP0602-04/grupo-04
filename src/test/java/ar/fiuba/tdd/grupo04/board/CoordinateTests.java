614d7704d82d2129d1366d697f57aff14b8ee1f7
mtejedor <mtejedor@hexacta.com>
2016-10-20 00:04:03 -0300

    improves in Coordinates Tests

--- src/test/java/ar/fiuba/tdd/grupo04/board/CoordinateTests.java
+++ src/test/java/ar/fiuba/tdd/grupo04/board/CoordinateTests.java
@@ -1,5 +1,6 @@
 package ar.fiuba.tdd.grupo04.board;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,39 +9,33 @@ import static org.junit.Assert.assertTrue;
 
 
 public class CoordinateTests {
+    private Coordinate coordinateR5C10;
+    private Coordinate coordinateR10C5;
+
+    @Before
+    public void init() {
+        coordinateR5C10 = new Coordinate(5, 10);
+        coordinateR10C5 = new Coordinate(10, 5);
+    }
 
     @Test
     public void testGetColumn() {
-        Integer col = 10;
-        Integer row = 5;
-        Coordinate newCoordinate = new Coordinate(row,col);
-        assertEquals(newCoordinate.column(), col);
+        assertEquals(coordinateR5C10.column(), (Integer)10);
     }
 
     @Test
     public void testGetRow() {
-        Integer col = 10;
-        Integer row = 5;
-        Coordinate newCoordinate = new Coordinate(row,col);
-        assertEquals(newCoordinate.row(), row);
+        assertEquals(coordinateR5C10.row(), (Integer)5);
     }
 
     @Test
-    public void testTwoEqualCordinates() {
-        Integer col = 10;
-        Integer row = 5;
-        Coordinate newCoordinate = new Coordinate(row,col);
-        Coordinate newCoordinate2 = new Coordinate(row,col);
-        assertTrue(newCoordinate.equals(newCoordinate2));
+    public void testTwoEqualCoordinates() {
+        assertTrue(coordinateR5C10.equals(new Coordinate(5, 10)));
     }
 
     @Test
-    public void testTwoDiferentCordinates() {
-        Integer col = 10;
-        Integer row = 5;
-        Coordinate newCoordinate = new Coordinate(row,col);
-        Coordinate newCoordinate2 = new Coordinate(col,row);
-        assertFalse(newCoordinate.equals(newCoordinate2));
+    public void testTwoDifferentCoordinates() {
+        assertFalse(coordinateR5C10.equals(coordinateR10C5));
     }
 
 }
