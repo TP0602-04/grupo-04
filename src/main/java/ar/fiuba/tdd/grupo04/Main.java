20a3cd837ccf708216227eae4ec18eaa244b8bee
Venedictto <mavenesio@gmail.com>
2016-10-20 15:50:51 -0200

    Fixed some checkStyles from Main Class

--- src/main/java/ar/fiuba/tdd/grupo04/Main.java
+++ src/main/java/ar/fiuba/tdd/grupo04/Main.java
@@ -1,11 +1,13 @@
 package ar.fiuba.tdd.grupo04;
 
+import com.google.gson.Gson;
+
 import ar.fiuba.tdd.grupo04.gui.GameGui;
 import ar.fiuba.tdd.grupo04.json.model.JsonGame;
 import ar.fiuba.tdd.grupo04.json.model.JsonInitGame;
 import ar.fiuba.tdd.grupo04.json.parser.GameJsonParser;
 import ar.fiuba.tdd.grupo04.util.FileUtils;
-import com.google.gson.Gson;
+
 
 public class Main {
     private static final String SUDOKU_PATH = "./src/main/resources/config/sudoku.json";
@@ -14,21 +16,13 @@ public class Main {
 
     public static void main(String[] args) {
         String json1 = FileUtils.readFile(SUDOKU_PATH);
-        if (json1 == null) {
-            System.out.println("CONFIGURATION FILE DOESN'T EXISTS!");
-            return;
-        }
-        String json2 = FileUtils.readFile(SUDOKU_MODEL_PATH);
-        if (json2 == null) {
-            System.out.println("CONFIGURATION FILE DOESN'T EXISTS!");
-            return;
-        }
         String json3 = FileUtils.readFile(SUDOKU_INIT_PATH);
-        if (json3 == null) {
+        String json2 = FileUtils.readFile(SUDOKU_MODEL_PATH);
+
+        if (json1 == null || json2 == null || json3 == null) {
             System.out.println("CONFIGURATION FILE DOESN'T EXISTS!");
             return;
         }
-
         Gson gson = new Gson();
         JsonGame jsonGame = gson.fromJson(json2, JsonGame.class);
         IGame game = null;
